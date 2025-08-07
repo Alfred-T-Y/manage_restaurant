2186236568b43563b2297bcc18f73d2e82c61874
Alfred-T-Y <tchoppeyonaalfred@gmail.com>
2025-08-07 20:19:17 +0100

    getx theme_manager with getstorage  added

--- lib/themes/theme_manager.dart
+++ lib/themes/theme_manager.dart
@@ -1,26 +1,25 @@
 import 'package:get/get.dart';
 import 'package:manage_restaurant/themes/dark_mode.dart';
 import 'package:manage_restaurant/themes/light_mode.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get_storage/get_storage.dart';
 
 class ThemeManager extends GetxController {
-
   var theme = lightMode.obs;
 
-  void loadMode() async {
-    final prefs = await SharedPreferences.getInstance();
-    (prefs.getBool('mode') ?? true) ?
+  void loadMode() {
+    final box = GetStorage();
+    (box.read('mode') ?? true) ?
     (theme.value=lightMode) : (theme.value=darkMode);
   }
 
 
-  Future<void> toggleTheme() async {
-    final prefs = await SharedPreferences.getInstance();
-    if (prefs.getBool('mode')!){
-      prefs.setBool('mode', false);
+  void toggleTheme() {
+    final box = GetStorage();
+    if (box.read('mode') ?? true){
+      box.write('mode', false);
       theme.value = darkMode;
     } else{
-      prefs.setBool('mode', true);
+      box.write('mode', true);
       theme.value = lightMode;
     }  
   }
